6f40f94e04531757e6889f843464d639311d3630
leandromacedosilva <iamfulldeveloper@gmail.com>
2022-06-25 15:30:31 -0300

    updated file api.js with params, query params and body params

--- backend/src/server/api.js
+++ backend/src/server/api.js
@@ -1,17 +1,45 @@
 // import do framework EXPRESS para o código
+const { response } = require('express');
 const express = require('express');
 
-// atribuição do método EXPRESS() a uma VAR
+// atribuição do método EXPRESS() a uma VAR.
 const app = express();
+
+//import middleware for express middlewares
+app.use(express.json());
 
 /* usando o método GET para trazer os dados */
 app.get('/products', (request, response) => {
-    response.json(['Biscoito','Farinha','Sal']);
+    // testing QUERY PARAMS
+    const query = request.query;
+    console.log(query);
+    return response.json(['Biscoito', 'Farinha', 'Sal']);
 })
 
 /* usando o método POST para atualizar os itens da lista */
 app.post('/products', (request, response) => {
-    response.json(['Biscoito','Farinha','Sal', 'Leite']);
+    // testing BODY PARAMS
+    const body = request.body;
+    console.log(body);
+    return response.json(['Biscoito', 'Farinha', 'Sal', 'Leite']);
+});
+
+/* usando o método PUT para alterar/atualizar item na lista */
+app.put('/products/:id', (request, response) => {
+    // testing ROUTE PARAMS
+    const params = request.params;
+    console.log(params);
+    return response.json(['Biscoito', 'Sardinha', 'Sal', 'Leite']);
+});
+
+/* usando o método PATCH altera/atualiza item na lista */
+app.patch('/products/:id', (request, response) => {
+    return response.json(['Biscoito', 'Sardinha', 'Amburguer', 'Leite']);
+});
+
+/* o método DELETE exclui item da lista */
+app.delete('/products/:id', (request, response) => {
+    return response.json(['Biscoito', 'Farinha', 'Sal', 'Macarrao']);  
 });
 
 // atribuição da porta onde a plicação irá responder
