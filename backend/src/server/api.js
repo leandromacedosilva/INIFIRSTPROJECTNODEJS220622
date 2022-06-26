f7413d77c754824b8f22a82de0d46a0908516a48
leandromacedosilva <iamfulldeveloper@gmail.com>
2022-06-26 09:35:21 -0300

    updated file api.js

--- backend/src/server/api.js
+++ backend/src/server/api.js
@@ -1,5 +1,5 @@
 // import do framework EXPRESS para o código
-const { response } = require('express');
+const { request, response } = require('express');
 const express = require('express');
 
 // atribuição do método EXPRESS() a uma VAR.
@@ -8,6 +8,15 @@ const app = express();
 //import middleware for express middlewares
 app.use(express.json());
 
+// array com dados para uso em tempo de execução. 
+const customers = [];
+
+app.post('/account', (request, response) => {
+    /* var cpf e name usando desestruturação */
+    const { cpf, name } = request.body;
+    return response.status(200).send();
+});
+
 /* usando o método GET para trazer os dados */
 app.get('/products', (request, response) => {
     // testing QUERY PARAMS
@@ -15,14 +24,6 @@ app.get('/products', (request, response) => {
     console.log(query);
     return response.json(['Biscoito', 'Farinha', 'Sal']);
 })
-
-/* usando o método POST para atualizar os itens da lista */
-app.post('/products', (request, response) => {
-    // testing BODY PARAMS
-    const body = request.body;
-    console.log(body);
-    return response.json(['Biscoito', 'Farinha', 'Sal', 'Leite']);
-});
 
 /* usando o método PUT para alterar/atualizar item na lista */
 app.put('/products/:id', (request, response) => {
