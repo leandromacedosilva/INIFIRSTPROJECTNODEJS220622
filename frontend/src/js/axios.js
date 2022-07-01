cb409e59fd37bbc301724b817ec5a9af2eb73625
leandromacedosilva <iamfulldeveloper@gmail.com>
2022-07-01 19:06:20 -0300

    added file in directory frontend/src/js/axios.js

--- frontend/src/js/axios.js
+++ frontend/src/js/axios.js
@@ -1,11 +1,11 @@
 // GET request for remote image in node.js
 axios({
     method: 'get',
-    url: 'http://127.0.0.1:9889',
+    url: 'http://127.0.0.1:7777/clients',
     responseType: 'stream'
   })
     .then(function (response) {
-        const datainf = response.data.data;
-        console.log(datainf);
+        const clients = response.data.data;
+        console.log(clients);
       //response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
     });
